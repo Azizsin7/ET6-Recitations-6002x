5d3ac35c64d166929f87ede9f94d18c43d26cff1
SomiZ <116781266+SomiZ@users.noreply.github.com>
2025-04-29 14:56:59 -0400

    Update Problem2.py

--- Session_05_MidtermStudy/Problem2.py
+++ Session_05_MidtermStudy/Problem2.py
@@ -15,9 +15,6 @@
 def max_contig_sum(L):
     """ L, a list of integers, at least one positive
     Returns the maximum sum of a contiguous subsequence in L """
-    def max_contig_sum(L):
-    """ L, a list of integers, at least one positive
-    Returns the maximum sum of a contiguous subsequence in L """
     max_ending_here = max_so_far = L[0]
     for i in L[1:]:
         max_ending_here = max(i, max_ending_here + i)
